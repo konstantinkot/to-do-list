f1fb823abe7f3478d76bad6b9afa448ffd4a02c0
= <=>
2020-10-10 01:58:24 +0300

    chancel cache

---
+++ precache-manifest.dba0f242e1bbf98bae3102ca2e90e78a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2b191f556393260961a5f10036c118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67704d50c18024a0438b",
+    "url": "/static/css/main.e27b51a0.chunk.css"
+  },
+  {
+    "revision": "8a38fcb0aee98a2bff78",
+    "url": "/static/js/2.d1e86f40.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.d1e86f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67704d50c18024a0438b",
+    "url": "/static/js/main.67dfe5c1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
--- src/utils.js
+++
@@ -1,2 +0,0 @@
-export const withoutIndex = (array, index) =>
-array.slice(0, index).concat(array.slice(index + 1));
